3641c9a906ee43cbaa5d6be8d1d18a03e6177845
Aliaksandr <lipskialiakasandr@gmail.com>
2021-05-20 09:29:19 +0300

    general view and components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+      <header className="header">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/480px-Instagram_logo_2016.svg.png"></img>
+      </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+      <div className="content">
+        <div>
+        <img src='https://s1.1zoom.ru/big0/235/Poppies_Summer_Grasslands_Trees_562184_1270x1024.jpg'></img>
+        </div>
+        <div>
+          ava + descript
+        </div>
+        <div>
+          My post
+          <div>
+            New post
+          </div>
+          <div>
+            Post1
+          </div>
+          <div>
+            Post2
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Profile;
