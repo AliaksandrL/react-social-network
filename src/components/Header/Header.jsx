e59ccf8f2ef86140148a5429cb22af0bd0ce6cb5
Aliaksandr <lipskialiakasandr@gmail.com>
2021-05-20 15:48:19 +0300

    folder structure, new components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-      <header className="header">
+      <header className={s.header}>
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/480px-Instagram_logo_2016.svg.png"></img>
       </header>
     );
