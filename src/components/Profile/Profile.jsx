706235f4a82383fd2ff7f1f1c9ceb4cf8708ca48
Aliaksandr <lipskialiakasandr@gmail.com>
2021-05-20 15:53:00 +0300

    folder structure, new components

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import MyPosts from "./MyPosts/MyPosts";
+import s from "./Profile.module.css";
+
+const Profile = () => {
+  return (
+    <div className={s.content}>
+      <div>
+        <img src="https://s1.1zoom.ru/big0/235/Poppies_Summer_Grasslands_Trees_562184_1270x1024.jpg"></img>
+      </div>
+      <div>ava + descript</div>
+      <MyPosts />
+    </div>
+  );
+};
+
+export default Profile;
