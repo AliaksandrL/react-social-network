5893458c1ce3b30975997cc7e91b09598eccf9cc
Aliaksandr <lipskialiakasandr@gmail.com>
2021-05-21 10:02:17 +0300

    add Profile

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import s from "./Post.module.css";
+
+const Post = (props) => {
+  return (
+    <div className={s.item}>
+      <img src="https://pbs.twimg.com/profile_images/1173161429266030592/lJCNA_JC.jpg" />
+      {props.message}
+      <div>
+        <span>{props.colLike}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
