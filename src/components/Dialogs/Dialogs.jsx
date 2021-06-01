e7d2137ff616e1b66bf680a46d89cc9de835783b
Aliaksandr <lipskialiakasandr@gmail.com>
2021-06-01 23:03:01 +0300

    added map to MyPosts and Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -16,20 +16,36 @@ const Message = (props) => {
 };
 
 const Dialogs = (props) => {
+
+  let dialogs = [
+    { id: 1, name: "Dimych" },
+    { id: 2, name: "Sasha" },
+    { id: 3, name: "Nastya" },
+    { id: 4, name: "Zhenya" },
+    { id: 5, name: "Valera" },
+  ]
+
+  let messages = [
+    { id: 1, message: "Hi" },
+    { id: 2, message: "How is your it?" },
+    { id: 3, message: "Yes!" },
+    { id: 4, message: "No" },
+    { id: 5, message: "Yoo" },
+  ]
+
+    let dialogsElements = dialogs
+    .map( d => <DialogItem name={d.name} id={d.id} /> );
+
+    let messagesElements = messages
+    .map( m => <Message message={m.message} /> );
+
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <DialogItem name="Dimych" id="1" />
-        <DialogItem name="Sasha" id="2" />
-        <DialogItem name="Nastya" id="3" />
-        <DialogItem name="Zhenya" id="4" />
-        <DialogItem name="Valera" id="5" />
+        { dialogsElements }             
       </div>
       <div className={s.messages}>
-        <Message message="Hi" />
-        <Message message="How is your it?" />
-        <Message message="Yes!" />
-        <Message message="Yes!" />
+     { messagesElements }
       </div>
     </div>
   );
