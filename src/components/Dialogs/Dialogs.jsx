c76c302151f6129879f23518081b85f577865d2d
Aliaksandr <lipskialiakasandr@gmail.com>
2021-06-09 21:22:08 +0300

    we throw callback through props

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -12,8 +12,23 @@ const Dialogs = (props) => {
     let messagesElements = props.state.messages
     .map( m => <Message message={m.message} /> );
 
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+      let text = newPostElement.current.value;
+      alert(text);
+    };
+
   return (
     <div className={s.dialogs}>
+      <div>
+        <div>
+          <textarea ref={newPostElement}></textarea>
+        </div>
+        <div>
+          <button onClick={addPost}>Add post</button>
+        </div>
+      </div>
       <div className={s.dialogsItems}>
         { dialogsElements }             
       </div>
