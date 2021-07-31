4ca1b784052f7129ef3de5b5493baf525d07970c
Aliaksandr <lipskialiakasandr@gmail.com>
2021-07-31 17:17:44 +0300

    changed state with adding reducer

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,79 +1,54 @@
-const ADD_POST = 'ADD-POST';
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
-
-const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
-const SEND_MESSAGE = 'SEND_MESSAGE';
+import {profileReducer} from "./profile-reducer";
+import {dialogsReducer} from "./dialogs-reducer";
+import {sidebarReducer} from "./sidebar-reducer";
 
 let store = {
-  _state: {
-    profilePage: {
-      posts: [
-        { id: 1, message: "Hi, how are you?", likesCount: 12 },
-        { id: 2, message: "It's my first post", likesCount: 11 },
-      ],
-      newPostText: "it.com",
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: "Hi, how are you?", likesCount: 12},
+                {id: 2, message: "It's my first post", likesCount: 11},
+            ],
+            newPostText: "it.com",
+        },
+        messagesPage: {
+            dialogs: [
+                {id: 1, name: "Dimych"},
+                {id: 2, name: "Sasha"},
+                {id: 3, name: "Nastya"},
+                {id: 4, name: "Zhenya"},
+                {id: 5, name: "Valera"},
+            ],
+            messages: [
+                {id: 1, message: "Hi"},
+                {id: 2, message: "How is your it?"},
+                {id: 3, message: "Yes!"},
+                {id: 4, message: "No"},
+                {id: 5, message: "Yoo"},
+            ],
+            newMessageBody: ""
+        },
+        sidebar: {}
     },
-    messagesPage: {
-      dialogs: [
-        { id: 1, name: "Dimych" },
-        { id: 2, name: "Sasha" },
-        { id: 3, name: "Nastya" },
-        { id: 4, name: "Zhenya" },
-        { id: 5, name: "Valera" },
-      ],
-      messages: [
-        { id: 1, message: "Hi" },
-        { id: 2, message: "How is your it?" },
-        { id: 3, message: "Yes!" },
-        { id: 4, message: "No" },
-        { id: 5, message: "Yoo" },
-      ],
-      newMessageBody: ""
+    _callSubscriber() {
+        console.log("State changed");
     },
-  },
-  _callSubscriber() {
-    console.log("State changed");
-  },
-  getState() {
-    return this._state;
-  },
-  subscribe(observer) {
-    this._callSubscriber = observer;
-  },
+    getState() {
+        return this._state;
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    },
 
-  dispatch(action) {
-    if (action.type === ADD_POST) {
-      let newPost = {
-        id: 5,
-        message: this._state.profilePage.newPostText,
-        likesCount: 0,
-      };
+    dispatch(action) {
 
-      this._state.profilePage.posts.push(newPost);
-      this._state.profilePage.newPostText = "";
-      this._callSubscriber(this._state);
-    } else if (action.type === UPDATE_NEW_POST_TEXT) {
-      this._state.profilePage.newPostText = action.newText;
-      this._callSubscriber(this._state);
-    } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-      this._state.messagesPage.newMessageBody = action.body;
-      this._callSubscriber(this._state);
-    } else if (action.type === SEND_MESSAGE) {
-      let body = this._state.messagesPage.newMessageBody;
-      this._state.messagesPage.newMessageBody = '';
-      this._state.messagesPage.messages.push({ id: 6, message: body });
-      this._callSubscriber(this._state);
+        this._state.profilePage = profileReducer(this._state.profilePage, action);
+        this._state.messagesPage = dialogsReducer(this._state.messagesPage, action);
+        this._state.sidebar = sidebarReducer(this._state.sidebar, action);
+
+        this._callSubscriber(this._state);
     }
-  },
-};
-
-export const addPostActionCreator = () => ({type: ADD_POST})
-export const updateNewPostTextActionCreator = (text) => 
-({type: UPDATE_NEW_POST_TEXT, newText: text})
-
-export const sendMessageCreator = () => ({type: SEND_MESSAGE})
-export const updateNewMessageBodyCreator = (body) => 
-({type: UPDATE_NEW_MESSAGE_BODY, newText: body})
+}
 
 export default store;
 window.state = store;
