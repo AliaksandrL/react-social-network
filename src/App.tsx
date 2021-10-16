0819760e4de64c2147382d007ef511ca225b697f
Aliaksandr <lipskialiakasandr@gmail.com>
2021-10-16 23:20:25 +0300

    example Preloader, loader-gif, isFetching indication

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Route } from "react-router-dom";
 import "./App.css";
-import Dialogs from "./components/Dialogs/Dialogs";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import News from "./components/News/News";
 import Profile from "./components/Profile/Profile";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import Users from "./components/Users/Users";
 import UsersContainer from "./components/Users/UsersContainer";
 
 const App = (props: any) => {
