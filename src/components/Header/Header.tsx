ae8874fb547734f27a7fda6d4d69dbac5aa16243
Aliaksandr <lipskialiakasandr@gmail.com>
2021-10-18 22:12:59 +0300

    cookie, login, auth/me

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import s from './Header.module.css';
 
 const Header = (props: any) => {
@@ -5,6 +6,11 @@ const Header = (props: any) => {
         <header className={s.header}>
             <img
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/480px-Instagram_logo_2016.svg.png"></img>
+
+            <div className={s.loginBlock}>
+                <NavLink to={'/login'}>Login</NavLink>
+            </div>
+
         </header>
     );
 }
