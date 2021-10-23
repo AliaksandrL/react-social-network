ad8ca0f9dd01209a07c06b2ae5489f0201f86650
Aliaksandr <lipskialiakasandr@gmail.com>
2021-10-23 20:39:08 +0300

    redux-thunk

--- src/api/api.js
+++ src/api/api.js
@@ -18,15 +18,21 @@ export const usersAPI = {
             .then(response => {
                 return response.data;
             });
-    }
+    },
+    follow(userId) {
+        instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+    },
+    unfollow(userId) {
+        return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+    },
 }
 
-export const getFollow = (currentPage = 1, pageSize = 10) => {
-    return instance.get(`follow?page=${currentPage}&count=${pageSize}`,
-        {
-            withCredentials: true,
-        })
-        .then(response => {
-            return response.data;
-        });
-}
+// export const getFollow = (currentPage = 1, pageSize = 10) => {
+//     return instance.get(`follow?page=${currentPage}&count=${pageSize}`,
+//         {
+//             withCredentials: true,
+//         })
+//         .then(response => {
+//             return response.data;
+//         });
+// }
