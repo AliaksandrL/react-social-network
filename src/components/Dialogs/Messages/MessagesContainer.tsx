12f55663f6e2beb95e04af76d26eeed93f4e13f5
AliaksandrL <lipskialiaksandr@gmail.com>
2022-02-05 21:54:54 +0300

    add ts Message

---
+++ src/components/Dialogs/Messages/MessagesContainer.tsx
@@ -0,0 +1,31 @@
+import {connect} from "react-redux";
+import {Messages} from "./Messages";
+import {messageTypes} from "../../../redux/DialogsReducer";
+import {AppStateTypes} from "../../../redux/store";
+import {compose} from "redux";
+import withAuthRedirect from "../../../hoc/withAuthRedirect";
+import React from "react";
+
+type mapStateToPropsType = {
+    messages: messageTypes[]
+}
+type mapDispatchToPropsType = {
+
+}
+export type MessagesPropsType = mapStateToPropsType & mapDispatchToPropsType
+
+const mapStateToProps = (state: AppStateTypes): mapStateToPropsType => {
+    return {
+        messages: state.dialogsPage.messages
+    }
+}
+const mapDispatchToProps = (): mapDispatchToPropsType => {
+    return {
+
+    }
+}
+
+export default compose<React.ComponentType>(
+    connect(mapStateToProps, mapDispatchToProps),
+    withAuthRedirect
+)(Messages)
